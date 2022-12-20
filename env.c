dc4803228b9a282ea3eaed08714276c673c7544b
Gunta Skrastina <gskrasti@c2s9d33.42wolfsburg.de>
2022-12-20 16:26:41 +0100

    handled env variable expansion

--- env.c
+++ env.c
@@ -6,7 +6,7 @@
 /*   By: gskrasti <gskrasti@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 19:17:42 by gskrasti          #+#    #+#             */
-/*   Updated: 2022/12/18 18:25:00 by gskrasti         ###   ########.fr       */
+/*   Updated: 2022/12/20 16:24:22 by gskrasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,14 @@ char	*replace(char *str, int i)
 
 	while (str[++i])
 	{
+		if (ft_charcmp(str, '$') < 0)
+			return (str);
+		if (ft_charcmp(str, '$') < i)
+			i = ft_charcmp(str, '$');
 		if (str[i] == '$')
-			replace_env(str, i, 0);
-		else if ((str[i] == '\'' || str[i] == '"') && ft_charcmp(str + i + 1, str[i]) >= 0)
+			str = replace_env(str, i, 0);
+		else if ((str[i] == '\'' || str[i] == '"')
+			&& ft_charcmp(str + i + 1, str[i]) >= 0)
 		{
 			quote = str[i++];
 			while (str[i] && str[i] != quote)
@@ -30,7 +35,7 @@ char	*replace(char *str, int i)
 				i++;
 			}
 		}
-		replace(str, i);
+		str = replace(str, i);
 	}
 	return (str);
 }
@@ -38,39 +43,59 @@ char	*replace(char *str, int i)
 char	*replace_env(char *str, int i, char quote)
 {
 	int		j;
+	char	*env;
 
-	if (!env_exists(str))
+	j = 0;
+	i++;
+	if (str[i] == ' ')
+		return (str);
+	env = ft_calloc(ft_strlen(str + i) + 1, sizeof(char *));
+	if (ft_isdigit(str[i]))
 	{
-		j = i + 1;
-		str[i] = '\0';
-		if (quote == '"')
-			while (str[j] && str[j] != quote && str[j] != ' ')
-				j++;
-		else
+		while (ft_isdigit(str[i + j]))
 		{
-			while (str[j])
-			{
-				if (((str[j] == '\'' || str[j] == '"')
-					&& ft_charcmp(str + j + 1, str[j]) >= 0) || str[j] == ' ')
-				{
-					ft_strlcat(str, str + j, ft_strlen(str) + ft_strlen(str + j) + 1);
-					return (str);
-				}
-				j++;
-			}
+			env[j] = str[i + j];
+			j++;
 		}
+		return (ft_new_str(str, --i, j, env));
 	}
-	else
+	while (str[i + j] && str[i + j] != '\0' && str[i + j] != ' '
+		&& str[i + j] != quote && str[i + j] != '$')
 	{
-		j = 0;
+		if (((str[i + j] == '\'' || str[i + j] == '"')
+				&& ft_charcmp(str + i + j, str[i + j]) >= 0))
+			return (ft_new_str(str, --i, j, env));
+		env[j] = str[i + j];
+		j++;
 	}
-	ft_strlcat(str, str + j, ft_strlen(str) + ft_strlen(str + j) + 1);
-	return (str);
+	return (ft_new_str(str, --i, j, env));
 }
 
-char	*env_exists(char *str)
+char	*ft_new_str(char *str, int i, int j, char *env)
 {
-	if (str)
-		return (NULL);
-	return (NULL);
+	char	*temp_begin;
+	char	*temp_end;
+	char	*new_env;
+
+	temp_begin = ft_calloc(i + 1, sizeof(char *));
+	temp_end = ft_calloc(ft_strlen(str + j) + 1, sizeof(char *));
+	ft_memcpy(temp_begin, str, i);
+	ft_memcpy(temp_end, str + i + ft_strlen(env) + 1, ft_strlen(str + j));
+	if (getenv(env))
+	{
+		new_env = ft_calloc(ft_strlen(getenv(env)) + 1, sizeof(char *));
+		new_env = getenv(env);
+	}
+	else
+		new_env = ft_calloc(1, sizeof(char *));
+	free (str);
+	str = ft_strjoin(temp_begin, new_env);
+	str = ft_strjoin(str, temp_end);
+	free(temp_begin);
+	free(temp_end);
+	// if (new_env)
+	// 	free(new_env);
+	free(env);
+	printf("%s\n", new_env);
+	return (str);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: gskrasti <gskrasti@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 14:03:28 by gskrasti          #+#    #+#             */
-/*   Updated: 2022/12/18 18:39:55 by gskrasti         ###   ########.fr       */
+/*   Updated: 2022/12/20 14:11:15 by gskrasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,12 @@
 # include <stdio.h>
 # include <readline/readline.h>
 # include <readline/history.h>
+# include <unistd.h>
 
 char	*replace(char *str, int i);
 char	*replace_env(char *str, int i, char quote);
 int		ft_charcmp(char *str, char c);
-char	*env_exists(char *str);
+// char	*env_exists(char *str);
+char	*ft_new_str(char *str, int i, int j, char *env);
 
 #endif

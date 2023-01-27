2a517207a81ff0bbb23d11ae7bb6101289dad627
Gunta Skrastina <gunta@Guntas-MacBook-Air.local>
2023-01-27 22:43:57 +0100

    fixed echo with n flag and no variables

--- echo.c
+++ echo.c
@@ -6,7 +6,7 @@
 /*   By: gskrasti <gskrasti@students.42wolfsburg    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 22:39:05 by gskrasti          #+#    #+#             */
-/*   Updated: 2023/01/16 14:41:50 by gskrasti         ###   ########.fr       */
+/*   Updated: 2023/01/27 22:42:39 by gskrasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,7 @@ void	echo(char *str)
 	int	i;
 
 	i = 0;
-	if (ft_strncmp(str, "-n ", 3))
-	{
-		while (str[i])
-		{
-			if (str[i] != '"' && str[i] != '\'')
-				ft_putchar_fd(str[i], 1);
-			i++;
-		}
-		ft_putchar_fd('\n', 1);
-	}
-	else
+	if (!ft_strncmp(str, "-n ", 3) || (!ft_strncmp(str, "-n", 2) && ft_strlen(str) == 2))
 	{
 		i = 3;
 		while (str[i])
@@ -36,5 +26,15 @@ void	echo(char *str)
 				ft_putchar_fd(str[i], 1);
 			i++;
 		}
+	}
+	else
+	{
+		while (str[i])
+		{
+			if (str[i] != '"' && str[i] != '\'')
+				ft_putchar_fd(str[i], 1);
+			i++;
+		}
+		ft_putchar_fd('\n', 1);
 	}
 }
